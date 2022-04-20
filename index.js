4506a9791f66b79c9cddb6adff5eab801a3e9583
Aldo Lamas <aldolamas01@gmail.com>
2022-04-19 22:20:31 -0500

    commit tree cd59564020189ec3002f7b268f731ca9089cd8a5

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
 }
 
-const mondayWork = function (activity = "go to the office") {
-    return `This Monday, I will ${activity}.`
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective(visualFlair = "*") {
+function wrapAdjective(visualFlair = ("*")) {
     return function (adjective = "special") {
         return `You are ${visualFlair}${adjective}${visualFlair}!`
     }
